d21971a0b6ad50b3a61bf70cbe5bea8e642c16ef
Yunika-Bajracharya <yunibajraa@gmail.com>
2023-12-25 11:12:54 +0545

    Configure CORS

--- server/main.py
+++ server/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from transformers import pipeline
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class QARequest(BaseModel):
     context: str
