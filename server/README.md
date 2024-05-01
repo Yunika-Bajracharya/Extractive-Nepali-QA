833ae370ce19a9ca0f7c83968a807eb67f0a3986
Suban Shrestha <subanshrestha244@gmail.com>
2024-05-02 00:20:49 +0545

    set up server with required files and readme

---
+++ server/README.md
@@ -0,0 +1,6 @@
+sudo apt install pdf2image
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
+
+Get nep.traineddata from https://github.com/tesseract-ocr/tessdata/blob/main/nep.traineddata
+and store it in 
--- server/main.py
+++ server/main.py
@@ -4,6 +4,11 @@ from pydantic import BaseModel
 from transformers import pipeline
 from pathlib import Path
 import uvicorn
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
 # import fitz  # PyMuPDF
 # import pdfplumber
 # from pdfminer.high_level import extract_text
@@ -19,52 +24,60 @@ app = FastAPI()
 UPLOAD_DIR = Path() / "uploads"
 # docSearch.build_courpus_from_directory(UPLOAD_DIR)
 
+if not os.environ["ORIGIN"]:
+    allowed_origins = ["*"]
+else:
+    allowed_origins = [os.environ["ORIGIN"]]
+
+print(allowed_origins)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 # qa_pipeline = pipeline("question-answering", model="Yunika/muril-squad-nepali")
-qa_pipeline = pipeline("question-answering", model="suban244/muRIL-squad-nep-hi-translated-squad") 
+qa_pipeline = pipeline(
+    "question-answering", model="suban244/muRIL-squad-nep-hi-translated-squad"
+)
 
 
 class QRequest(BaseModel):
     question: str
-    
+
 
 class QWithContext(QRequest):
     context: str
     fileName: str
 
+
 class QWithUrl(QRequest):
     url: str
 
-def extract_text_from_pdf(file_path):
-    # text = ""
-    
-    # with pdfplumber.open(file_path) as pdf:
-    #     for page in pdf.pages:
-    #         text += page.extract_text()
-    
-    # PDF Miner
-    # text = extract_text(file_path)
-            
-    # return text
 
+def create_folder_if_not_exists(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created.")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
+
+
+def extract_text_from_pdf(file_path):
     pages = convert_from_path(file_path, 500)  # 500 dpi for higher quality conversion
 
     full_text = ""
 
     for page in pages:
         # Extract text from the image using Tesseract
-        text = pytesseract.image_to_string(page, lang='nep')
+        text = pytesseract.image_to_string(page, lang="nep")
 
         # Append the text to the full text string
         full_text += text
 
-    return(full_text)
+    return full_text
+
 
 @app.post("/answer")
 async def get_answer(qa_request: QWithContext):
@@ -72,7 +85,7 @@ async def get_answer(qa_request: QWithContext):
         file_path = UPLOAD_DIR / qa_request.fileName
         pdf_text = extract_text_from_pdf(file_path)
         qa_request.context = pdf_text
-        
+
     answer = qa_pipeline(question=qa_request.question, context=qa_request.context)  # type: ignore
     return {"answer": answer}
 
@@ -81,6 +94,7 @@ async def get_answer(qa_request: QWithContext):
 async def create_upload_file(file_upload: UploadFile):
     data = await file_upload.read()
     save_to = UPLOAD_DIR / (file_upload.filename or "untitled")
+    create_folder_if_not_exists(UPLOAD_DIR)
     with open(save_to, "wb") as f:
         f.write(data)
 
@@ -107,6 +121,11 @@ async def get_answer_by_scrapping(req: QWithUrl):
     answer = qa_pipeline(question=req.question, context="\n".join(paragraphs))  # type: ignore
     print(answer)
     return {"answer": answer}
+
+
+@app.get("/")
+async def hello():
+    return {"answer": "world"}
 
 
 if __name__ == "__main__":
